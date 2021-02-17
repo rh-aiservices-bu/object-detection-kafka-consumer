5b59f634bec4d10e5b418a58884907ff5efabfc9
Chris Chase <cchase@redhat.com>
2021-02-16 22:07:07 -0500

    Added SASL_SSL / PLAIN config

--- app.py
+++ app.py
@@ -3,27 +3,39 @@ import json
 from kafka import KafkaConsumer, KafkaProducer
 from prediction import predict
 
-KAFKA_BOOTSTRAP_SERVER = os.getenv('KAFKA_BROKER_LIST') or 'modh-demo-kafka-bootstrap:9092'
-CONSUMER_GROUP = 'object-detection-consumer-group'
-CONSUMER_TOPIC = os.getenv('KAFKA_TOPIC_IMAGES') or 'images'
-PRODUCER_TOPIC = os.getenv('KAFKA_TOPIC_OBJECTS') or 'objects'
+KAFKA_BOOTSTRAP_SERVER = os.getenv('KAFKA_BOOTSTRAP_SERVER')
+KAFKA_SECURITY_PROTOCOL = os.getenv('KAFKA_SECURITY_PROTOCOL')
+KAFKA_SASL_MECHANISM = os.getenv('KAFKA_SASL_MECHANISM')
+KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
+KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
+KAFKA_CONSUMER_GROUP = 'object-detection-consumer-group'
+KAFKA_CONSUMER_TOPIC = os.getenv('KAFKA_TOPIC_IMAGES')
+KAFKA_PRODUCER_TOPIC = os.getenv('KAFKA_TOPIC_OBJECTS')
 
 
 def main():
-    consumer = KafkaConsumer(CONSUMER_TOPIC,
-                             group_id=CONSUMER_GROUP,
-                             auto_offset_reset='earliest',
+    consumer = KafkaConsumer(KAFKA_CONSUMER_TOPIC,
+                             group_id=KAFKA_CONSUMER_GROUP,
                              bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
+                             security_protocol=KAFKA_SECURITY_PROTOCOL,
+                             sasl_mechanism=KAFKA_SASL_MECHANISM,
+                             sasl_plain_username=KAFKA_USERNAME,
+                             sasl_plain_password=KAFKA_PASSWORD,
+                             auto_offset_reset='earliest',
                              api_version_auto_timeout_ms=30000,
                              request_timeout_ms=450000)
 
     producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
+                             security_protocol=KAFKA_SECURITY_PROTOCOL,
+                             sasl_mechanism=KAFKA_SASL_MECHANISM,
+                             sasl_plain_username=KAFKA_USERNAME,
+                             sasl_plain_password=KAFKA_PASSWORD,
                              api_version_auto_timeout_ms=30000,
                              max_block_ms=900000,
                              request_timeout_ms=450000,
                              acks='all')
 
-    print(f'Subscribed to "{KAFKA_BOOTSTRAP_SERVER}" consuming topic "{CONSUMER_TOPIC}, producing messages on topic "{PRODUCER_TOPIC}"...')
+    print(f'Subscribed to "{KAFKA_BOOTSTRAP_SERVER}" consuming topic "{KAFKA_CONSUMER_TOPIC}, producing messages on topic "{KAFKA_PRODUCER_TOPIC}"...')
 
     try:
         for record in consumer:
@@ -31,7 +43,7 @@ def main():
             dict = json.loads(msg)
             result = predict(dict)
             dict['prediction'] = result
-            producer.send(PRODUCER_TOPIC, json.dumps(dict).encode('utf-8'))
+            producer.send(KAFKA_PRODUCER_TOPIC, json.dumps(dict).encode('utf-8'))
             producer.flush()
     finally:
         print("Closing KafkaTransformer...")
